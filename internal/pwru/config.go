50c3b9ccf3bac2076a0cec816c6ea35e72c5693c
Gray Liang <gray.liang@isovalent.com>
2023-09-26 13:31:05 +0800

    Add --filter-ifname
    Set this flag to filter skbs whose dev->ifindex equals to user specified
    one, under the netns set by --filter-netns.
    
    If --filter-netns is not set, pwru automatically detects current netns.
    
    Pwru has to switch netns in order to parse ifindex from ifname. To
    achieve that, --filer-netns accepts multi-forms arguments:
    1. By path: --filter-netns /proc/<pid>/ns/net
    2. By inode: --filter-netns inode:4026533332
    
    --filter-ifname can only work with --filter-netns by path, otherwise
    pwru cannot switch to target netns by inode.
    
    Signed-off-by: Zhichuan Liang <gray.liang@isovalent.com>

--- internal/pwru/config.go
+++ internal/pwru/config.go
@@ -4,12 +4,24 @@
 
 package pwru
 
+import (
+	"fmt"
+	"net"
+	"runtime"
+	"strconv"
+	"strings"
+
+	"github.com/vishvananda/netns"
+	"golang.org/x/sys/unix"
+)
+
 // Version is the pwru version and is set at compile time via LDFLAGS-
 var Version string = "version unknown"
 
 type FilterCfg struct {
-	FilterNetns uint32
-	FilterMark  uint32
+	FilterNetns   uint32
+	FilterMark    uint32
+	FilterIfindex uint32
 
 	// TODO: if there are more options later, then you can consider using a bit map
 	OutputRelativeTS uint8
@@ -22,11 +34,10 @@ type FilterCfg struct {
 	TrackSkb byte
 }
 
-func GetConfig(flags *Flags) FilterCfg {
-	cfg := FilterCfg{
-		FilterNetns: flags.FilterNetns,
-		FilterMark:  flags.FilterMark,
-		IsSet:       1,
+func GetConfig(flags *Flags) (cfg FilterCfg, err error) {
+	cfg = FilterCfg{
+		FilterMark: flags.FilterMark,
+		IsSet:      1,
 	}
 	if flags.OutputSkb {
 		cfg.OutputSkb = 1
@@ -40,10 +51,59 @@ func GetConfig(flags *Flags) FilterCfg {
 	if flags.OutputStack {
 		cfg.OutputStack = 1
 	}
-
 	if flags.FilterTrackSkb {
 		cfg.TrackSkb = 1
 	}
 
-	return cfg
+	if cfg.FilterIfindex, cfg.FilterNetns, err = parseIfindexAndNetns(flags.FilterIfname, flags.FilterNetns); err != nil {
+		return
+	}
+	fmt.Printf("FilterIfindex: %d, %d\n", cfg.FilterIfindex, cfg.FilterNetns)
+	return
+}
+
+func parseIfindexAndNetns(ifname, netnsSpecifier string) (ifindex, netnsID uint32, err error) {
+	var ns netns.NsHandle
+	switch {
+	case netnsSpecifier == "":
+		ns, err = netns.Get()
+	case strings.HasPrefix(netnsSpecifier, "/"):
+		ns, err = netns.GetFromPath(netnsSpecifier)
+	case strings.HasPrefix(netnsSpecifier, "inode:"):
+		if ifname != "" {
+			err = fmt.Errorf("inode netns specifier cannot be used with --filter-ifname")
+		} else {
+			netnsInode, err := strconv.Atoi(netnsSpecifier[6:])
+			return 0, uint32(netnsInode), err
+		}
+	default:
+		err = fmt.Errorf("invalid netns specifier: %s", netnsSpecifier)
+	}
+	if err != nil {
+		return
+	}
+
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	currentNetns, err := netns.Get()
+	if err != nil {
+		return
+	}
+	defer netns.Set(currentNetns)
+
+	if err = netns.Set(ns); err != nil {
+		return
+	}
+
+	iface, err := net.InterfaceByName(ifname)
+	if err != nil {
+		return
+	}
+
+	var s unix.Stat_t
+	if err = unix.Fstat(int(ns), &s); err != nil {
+		return
+	}
+	return uint32(iface.Index), uint32(s.Ino), nil
 }
